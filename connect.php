f3a3cb2e6fba3b1309b2823329e707aba8aa516d
spyrash <diego.petrucci@outlook.com>
2021-10-02 19:38:38 +0200

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+//connect.php
+$dbconn = pg_connect("host=localhost port=5432 dbname=forumCode user=postgres password=Softair97") 
+ or die('Errore di connessione: ' . pg_last_error());
+?>
+<!-- mettere in user il nome utente dell'utente che accede al db, password dell'utente e dbname il nome del database-->
