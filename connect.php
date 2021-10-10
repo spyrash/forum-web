a3290399228c5fb2172bc6722bd20b8512be744b
Diego <74570312+spyrash@users.noreply.github.com>
2021-10-10 15:35:02 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //connect.php
-$dbconn = pg_connect("host=localhost port=5432 dbname=forumCode user=postgres password=Softair97") 
+$dbconn = pg_connect("host=localhost port=5432 dbname=forumCode user=postgres password=1234") 
  or die('Errore di connessione: ' . pg_last_error());
 ?>
 <!-- mettere in user il nome utente dell'utente che accede al db, password dell'utente e dbname il nome del database-->
