87d862b9c2cd80a63bbb528ea553ef598d90bfc6
spyrash <diego.petrucci@outlook.com>
2022-02-01 19:08:19 +0100

    a

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <small>
             &emsp;Questo sito web è stato realizzato da Diego Petrucci e Angelo Garcia, studenti di Ingegneria Informatica dell'università Sapienza.<br>
             &emsp;Contatti email:&emsp;diego.petrucci@outlook.com<br>
-            &emsp;Repository GIT:&emsp;<a class="myAnchor" href="https://github.com/spyrash/forum-CodeToCode" target="_blank">github-forum-CodeToCode</a>
+            &emsp;Repository GIT:&emsp;<a class="myAnchor" href="https://github.com/spyrash/forum-web" target="_blank">github-forum-CodeToCode</a>
         </small>
     </div>
 
